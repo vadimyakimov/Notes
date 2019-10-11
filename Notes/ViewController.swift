9d18bbb62d97296e304615b4498f8a0b1596fee8
vadimyakimov <vadim_yakimov@me.com>
2019-10-11 11:05:01 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Вадим on 11.10.2019.
+//  Copyright © 2019 Вадим Якимов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
